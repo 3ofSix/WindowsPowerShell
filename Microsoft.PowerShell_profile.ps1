b97654df1a28be80a48a1f677494575be96ca2a1
Garrett Vernon <garrett.vernon@gmail.com>
2023-11-07 14:12:38 +0000

    Update Microsoft.PowerShell_profile.ps1
    Add: Set-PathVariable function
    Add: Alias np for notepad++

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,36 @@ Function GUID {
 	echo $guid
 	Set-Clipboard -Value $guid.Trim()
 }
+
+<#
+	.Synopsis
+	Add or remove a path
+	.Description
+	Add or remove a path to the PATH variable
+#>
+Function Set-PathVariable {
+    param (
+        [string]$AddPath,
+        [string]$RemovePath
+    )
+    $regexPaths = @()
+    if ($PSBoundParameters.Keys -contains 'AddPath'){
+        $regexPaths += [regex]::Escape($AddPath)
+    }
+
+    if ($PSBoundParameters.Keys -contains 'RemovePath'){
+        $regexPaths += [regex]::Escape($RemovePath)
+    }
+    
+    $arrPath = $env:Path -split ';'
+    foreach ($path in $regexPaths) {
+        $arrPath = $arrPath | Where-Object {$_ -notMatch "^$path\\?"}
+    }
+    $env:Path = ($arrPath + $addPath) -join ';'
+}
+
+# Set PATH to include notepad++
+Set-PathVariable -AddPath 'C:\Program Files\Notepad++\'
+
+# Alias
+Set-Alias -Name np -Value notepad++
